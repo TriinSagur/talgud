91f86ebc41589efd1b4de7fe271b64484c444eab
LauriSooro <lauri.sooro@gmail.com>
2022-05-03 10:17:39 +0300

    Contact uuendatud @Lauri

--- src/main/java/ee/bcs/talgud/domain/contact/ContactMapper.java
+++ src/main/java/ee/bcs/talgud/domain/contact/ContactMapper.java
@@ -11,8 +11,10 @@ public interface ContactMapper {
     @Mapping(source = "user.id", target = "userId")
     ContactDto contactToContactDto(Contact contact);
 
-    @Mapping(source = "userId", target = "user.id")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user.id", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateContactFromContactDto(ContactDto contactDto, @MappingTarget Contact contact);
+
 
 }
