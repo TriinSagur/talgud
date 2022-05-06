00fc8aa29c9c93f530146a83779f418b6222eead
priitrs <priitrs@gmail.com>
2022-05-06 11:48:44 +0300

    PRIIT-07 ProjectResponse fix

--- src/main/java/ee/bcs/talgud/domain/projectuser/ProjectUserRepository.java
+++ src/main/java/ee/bcs/talgud/domain/projectuser/ProjectUserRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProjectUserRepository extends JpaRepository<ProjectUser, Integer> {
-    @Query("select p from ProjectUser p where p.user.id = ?1")
-    List<ProjectUser> findByUser_Id(Integer id);
 
     @Query("select p from ProjectUser p where p.project.id = ?1")
     List<ProjectUser> findByProject_Id(Integer id);
+
+    @Query("select p from ProjectUser p where p.user.id = ?1 order by p.project.startTime")
+    List<ProjectUser> findByUser_Id(Integer id);
+
+
 }
